34d886585824a3cc2088fb2e01e73df95ba0d9b0
Erica Windisch <erica@iopipe.com>
2017-07-19 15:28:11 +0000

    s3, fixes, webpack
    Signed-off-by: Erica Windisch <erica@iopipe.com>

--- index.js
+++ index.js
@@ -1,26 +1,51 @@
 const lambdaCallback = require('./callback')
 const lambdaContext = require('./callback')
 const v8profiler = require('v8-profiler')
+const AWS = require('aws-sdk')
+const s3 = new AWS.S3();
 
 
 var profiler = {}
 
 
-profiler.constructor = (config) => {
+profiler.constructor = (userConfig) => {
+  var userConfig = (userConfig) ? userConfig : {}
+  config = {
+    s3bucket: userConfig.s3bucket || 'lambda-profiler-dumps',
+    recsamples: userConfig.recsamples || true,
+    sampleRate: userConfig.sampleRate || 100
+  }
   var decorator = (userFunction) => {
-    v8profiler.startProfiling()
-
     return (event, context, callback) => {
+      v8profiler.setSamplingInterval(config.sampleRate)
+      v8profiler.startProfiling(undefined, config.recsamples)
+
       var stopAndSend = new Promise ((resolve, reject) => {
-        var output = v8profiler.stopProfiling()
-        console.log(output)
-        resolve()
+        var profile = v8profiler.stopProfiling()
+        profile.export((err, output) => {
+          (err) ? reject(err) : s3.putObject({
+            Body: output,
+            Bucket: config.s3bucket,
+            Key: context.awsRequestId + ".cpuprofile"
+          }, (err, data) => {
+            if (err) {
+              reject(err)
+              return
+            }
+            resolve(data)
+          })
+        })
       })
 
       var newCallback = lambdaCallback(callback, stopAndSend)
       var newContext = lambdaContext(context, stopAndSend)
 
-      userFunction(event, context, callback)
+      try {
+        userFunction(event, context, callback)
+      }
+      catch (e) {
+        stopAndSend().then(() => { throw e })
+      }
     } 
   }
 
--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,21 @@ module.exports = {
     filename: "./index.js",
     libraryTarget: 'commonjs2',
     library: "lambda-profiler"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: ['shebang-loader', 'babel-loader']
+      },
+      {
+        test: /\.node$/,
+        use: 'node-loader'
+      },
+      {
+        test: /\.(h|.cc|.c|.o)$/,
+        use: 'ignore-loader'
+      }
+    ]
   }
 }
